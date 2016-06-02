d36389a80fbd937a147200df041c4ca2eae22792
Gloria Ma <gloria.ma@aya.yale.edu>
2016-06-02 10:56:50 -0700

    Add test for then method

--- test/no-chaining.test.js
+++ test/no-chaining.test.js
@@ -1,7 +1,31 @@
 var assert = require('chai').assert;
+var Promise = require('../src/no-chaining');
 
 describe('No Chaining', function() {
-  it('should pass a dummy test', function() {
-    assert.isTrue(true);
+  var promise;
+
+  beforeEach(function() {
+    promise = new Promise();
+  });
+
+  it('should have methods then, resolve, and reject', function() {
+    assert.isFunction(promise.then);
+    assert.isFunction(promise.resolve);
+    assert.isFunction(promise.reject);
+  });
+
+  describe('then', function() {
+    var callback;
+
+    beforeEach(function() {
+      callback = function(value) {
+        console.log(value);
+      };
+      promise.then(callback);
+    });
+
+    it('should push argument to functionsToCall', function() {
+      assert.equal(callback, promise.functionsToCall[0]);
+    });
   });
 });
